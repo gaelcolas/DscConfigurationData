f334a62439f8e7e24230125860857d03271afa84
gaelcolas <gaelcolas@users.noreply.github.com>
2017-01-06 00:02:17 +0000

    Adding help to Export-DscCredentialFile

--- DscConfiguration/Private/Export-DscCredentialFile.ps1
+++ DscConfiguration/Private/Export-DscCredentialFile.ps1
@@ -1,6 +1,31 @@
 #require -Module ProtectedData
 function Export-DscCredentialFile
 {
+    <#
+      .SYNOPSIS
+      Create a PSD1 file with a list of UserName = EncryptedCredential
+    
+      .DESCRIPTION
+      This function encrypts the Credential parts of an hashtable looking like
+      @{'key1' = [PSCredential];'key2' = [PSCredential];}
+      before creating a PSD1 file out of this data, looking like:
+      @{
+          'UserName1' = 'MUvk0a1JIRz79Zw6B08nnMy4OG222pRBzvGxmQ6QCIVi7936kxfC3ATwMK...'
+          'UserName2' = 'crCcrLoQmFwI5wS7jm0B7To1M0HzQvlZVS4KKy22VJSk1rYJFtkBWisvwS...'
+      }
+    
+      .EXAMPLE
+      $secpasswd = ConvertTo-SecureString 'P@ssw0rd' -AsPlainText -Force
+      $mycreds = New-Object System.Management.Automation.PSCredential ('username', $secpasswd)
+      Export-DscCredentialFile -Hashtable (@{'username'=$secpasswd}) -Path C:\DSC_Configuration\Credentials\PasswordStore.encrypted.psd1
+    
+      .PARAMETER Hashtable
+      The Hashtable parameter is a lookup hashtable with the Username as key and the [PSCredential] as value.
+    
+      .PARAMETER Path
+      The Path parameter is the full Path where you want the encrypted password stored.
+      
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
