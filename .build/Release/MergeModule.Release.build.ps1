4cedcdf2b53ac10fe596f6067ebdc46945c00167
gaelcolas <gaelcolas@users.noreply.github.com>
2017-01-08 00:13:57 +0000

    Added draft examples

---
+++ .build/Release/MergeModule.Release.build.ps1
@@ -0,0 +1,26 @@
+﻿#From Chris Dent's BuildTask repo
+#https://github.com/indented-automation/BuildTools/blob/master/build_tasks.ps1
+
+Task MergeModule {
+    # Merge individual PS1 files into a single PSM1.
+    
+    # Use the same case as the manifest.
+    $moduleName = (Get-Item "source\$moduleName.psd1").BaseName
+
+    $fileStream = New-Object System.IO.FileStream("$projectPath\$Script:version\$moduleName.psm1", 'Create')
+    $writer = New-Object System.IO.StreamWriter($fileStream)
+
+    Get-ChildItem 'source' -Filter *.ps1 -Recurse | Where-Object { $_.FullName -notlike "*source\examples*" -and $_.Extension -eq '.ps1' } | ForEach-Object {
+        Get-Content $_.FullName | ForEach-Object {
+            $writer.WriteLine($_.TrimEnd())
+        }
+        $writer.WriteLine()
+    }
+
+    if (Test-Path 'source\InitializeModule.ps1') {
+        $writer.WriteLine('InitializeModule')
+    }
+
+    $writer.Close()
+    $fileStream.Close()
+}
